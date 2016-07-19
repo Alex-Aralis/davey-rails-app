5341c3e4b47a1d7a155ac0fbe944c23a72575fa2
Alex Aralis <alex.aralis@gmail.com>
2016-07-19 09:46:40 +0000

    have the /posts endpoint grab from https://bluitxtern.herokuapp.com/posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include HTTParty
+
+  base_uri "https://bluitxtern.herokuapp.com"  
+
+  def outside_posts
+    JSON.parse(self.class.get("/posts.json").body)
+  end
 end
